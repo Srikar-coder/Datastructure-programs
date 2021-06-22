645aac655d68a6cb545720157be2a71e6ada5220
Srikar <srikarsamanchi1999@gmail.com>
2021-06-22 10:54:19 +0530

    palindrome checker

---
+++ Utility.java
@@ -0,0 +1,51 @@
+package com.bridgeLabz.dataStructure;
+import java.io.*;
+public class Utility
+{
+	BufferedReader br;
+	//constructor to initialize bufferedReader
+	public Utility(){
+		br = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	//take input word
+	public String inputString(){
+		try{
+			return br.readLine();
+		}
+		catch(IOException ioe){
+			System.out.println(ioe.getMessage());
+		}
+		return "";
+	}
+
+	//Take Integer Input
+	public int inputInteger(){
+		try{
+			try{	
+				return Integer.parseInt(br.readLine());
+			}
+			catch(NumberFormatException nfe){
+				System.out.println(nfe.getMessage());	
+			}
+		}catch(IOException ioe){
+			System.out.println(ioe.getMessage());
+		}
+		return 0;
+	}
+
+	//Take Double Input
+	public double inputDouble(){
+		try{
+			try{	
+				return Double.parseDouble(br.readLine());
+			}
+			catch(NumberFormatException nfe){
+				System.out.println(nfe.getMessage());	
+			}
+		}catch(IOException ioe){
+			System.out.println(ioe.getMessage());
+		}
+		return 0.0;
+	}
+}
