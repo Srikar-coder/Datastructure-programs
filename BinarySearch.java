0824566351863d4dfe64634a269ab686b73df09a
Srikar <srikarsamanchi1999@gmail.com>
2021-06-22 11:04:13 +0530

    Binary search

---
+++ BinarySearch.java
@@ -0,0 +1,33 @@
+package com.bridgeLabz.dataStructurePrograms;
+import java.util.*;
+import java.io.*;
+public class BinarySearch 
+{
+	public static void main(String args[])
+	{
+		SortingFunction sf=new SortingFunction();  
+		Utility u=new Utility();
+
+		System.out.println("Enter size: ");
+		int arraySize=u.inputInteger();
+		int a[]=sf.input1DArray(arraySize);
+		Arrays.sort(a);								//sorting the array
+		System.out.println();
+		System.out.println("The Sorted Order is: ");
+		sf.print1DArray(a);							//printing the sorted array
+		System.out.println();
+		System.out.println("Enter Key: ");
+		int position=sf.binarySearch(a,u.inputInteger());
+
+		if(position==-1)
+		{
+			System.out.println();
+			System.out.println("Not Found");
+		}
+		else
+		{
+			System.out.println();
+			System.out.println("Found at "+position+" position");
+		}
+	}
+}
---
+++ BinarySearchString.java
@@ -0,0 +1,34 @@
+package com.bridgeLabz.dataStructurePrograms;
+import java.util.*;
+import java.io.*;
+public class BinarySearchString
+{      	    
+	public static void main(String[] args)
+	{
+		SortingFunction sf=new SortingFunction();  
+		Utility u=new Utility();
+		System.out.println("Enter number of words to be sorted: ");
+		int arraySize1=u.inputInteger();
+		String b[]=sf.input1DStringArray(arraySize1);
+		Arrays.sort(b);									//sorting the array
+		System.out.println();
+
+		System.out.println("The Sorted Order is: ");
+		sf.print1DStringArray(b);						//printing the sorted array
+		System.out.println();
+
+		System.out.println("Enter Key: ");
+		int p=sf.binarySearchString(b,u.inputString());
+
+		if(p==-1)
+		{
+			System.out.println();
+			System.out.println("Not Found");
+		}
+		else
+		{
+			System.out.println();
+			System.out.println("Found at "+p+" position");
+		}	        
+	}
+}
